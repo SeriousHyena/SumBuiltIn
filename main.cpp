df699822504353c64822cc9b12135376077265f3
SeriousHyena <emackiii@gmail.com>
2021-10-19 15:46:19 -0700

    final

--- main.cpp
+++ main.cpp
@@ -3,8 +3,32 @@
 
 using namespace std;
 
+//declare arrays here
+array<int, 5> myArray = {2,4,6,8,10};
+
+int collector ;
+int& sum = collector;
+
+//declare functions here
+int sumArray(int arrayInt, int arraySize);
+
 int main()
 {
+	int sumArray(int arrayInt, int arraySize);
+	int foo=sumArray(1, 2);
+	cout <<"The sum of the array integers is: " << sum<<endl;
 
 	return 0;
+}
+
+//place functions here
+int sumArray(int arrayInt, int arraySize)
+
+{
+	for (int i = 0; i < myArray.size(); i++)
+	{
+		sum += myArray[i];
+	}
+	
+	return sum;
 }
